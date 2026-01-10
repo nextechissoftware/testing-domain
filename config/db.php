ad9701e83951315e5cd4bba090e8f0d719cc89a4
bitboyss <saurabhsb88877@gmail.com>
2026-01-11 01:54:49 +0530

    [UPD] db.php

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "gallery_db");
+$conn = mysqli_connect("localhost", "u221387206_admin", "Mili@88877", "u221387206_shaheed_rns");
 
 if (!$conn) {
   die("Database connection failed");
